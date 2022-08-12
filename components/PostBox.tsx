0e8e2544d22511e551c7f7ca2cd65067f44fd541
Adam Andrews <andrewsadam07@gmail.com>
2022-08-12 16:50:27 +0100

    Configured react form

--- components/PostBox.tsx
+++ components/PostBox.tsx
@@ -5,22 +5,36 @@ import { PhotographIcon, LinkIcon } from '@heroicons/react/solid';
 import { useForm } from 'react-hook-form';
 
 type FormData = {
-    postTitle: string;
-    postBody: string;
-    postImage: string;
-    subreddit: string;
-}
+	postTitle: string;
+	postBody: string;
+	postImage: string;
+	subreddit: string;
+};
 function PostBox() {
 	const { data: session } = useSession();
+	const [imageBoxOpen, setImageBoxOpen] = React.useState<boolean>(false);
 
-    const {register,setValue,handleSubmit,watch,formState:{errors}} = useForm<FormData>();
+	const {
+		register,
+		setValue,
+		handleSubmit,
+		watch,
+		formState: { errors },
+	} = useForm<FormData>();
+
+	const onSubmit = handleSubmit(async (formData) => {
+		console.log(formData);
+	});
 	return (
-		<form className="sticky top-16 z-50 border rounded-md border-gray-300 bg-white p-2">
+		<form
+			onSubmit={onSubmit}
+			className="sticky top-16 z-50 border rounded-md border-gray-300 bg-white p-2"
+		>
 			<div className="flex items-center space-x-3">
 				{/* Avatar */}
 				<Avatar />
 				<input
-                {...register("postTitle",{required:true})}
+					{...register('postTitle', { required: true })}
 					disabled={!session}
 					className="flex-1 rounded-md bg-gray-50 p-2 pl-5 outline-none"
 					type="text"
@@ -28,30 +42,67 @@ function PostBox() {
 						session ? 'Create a post by entering a title' : 'Login to post'
 					}
 				/>
-				<PhotographIcon className={`h-6 cursor-pointer text-gray-300`} />
+				<PhotographIcon
+					onClick={() => setImageBoxOpen(!imageBoxOpen)}
+					className={`h-6 cursor-pointer text-gray-300 ${
+						imageBoxOpen && 'text-blue-300'
+					}`}
+				/>
 				<LinkIcon className="h-6 text-gray-300" />
 			</div>
 
-            {!!watch("postTitle") &&(
-                <div className='flex flex-col py-2'>
-                    <div className='flex items-center px-2'>
-                        <p className='min-w-[90px]'>Body:</p>
-                        <input className='m-2 flex-1 p-2 outline-none'
-                        {...register("postBody")}
-                        type="text"
-                        placeholder="Enter a body(Optional)"
-                        />
-                    </div>
-                    <div className='flex items-center px-2'>
-                        <p className='min-w-[90px]'>Subreddit:</p>
-                        <input className='m-2 flex-1 p-2 outline-none'
-                        {...register("subreddit")}
-                        type="text"
-                        placeholder="i.e Next.js"
-                        />
-                    </div>
-                </div>
-            )}
+			{!!watch('postTitle') && (
+				<div className="flex flex-col py-2">
+					<div className="flex items-center px-2">
+						<p className="min-w-[90px]">Body:</p>
+						<input
+							className="m-2 flex-1 p-2 outline-none"
+							{...register('postBody')}
+							type="text"
+							placeholder="Enter a body(Optional)"
+						/>
+					</div>
+					<div className="flex items-center px-2">
+						<p className="min-w-[90px]">Subreddit:</p>
+						<input
+							className="m-2 flex-1 p-2 outline-none"
+							{...register('subreddit', { required: true })}
+							type="text"
+							placeholder="i.e Next.js"
+						/>
+					</div>
+					{imageBoxOpen && (
+						<div className="flex items-center px-2">
+							<p className="min-w-[90px]">Image:</p>
+							<input
+								className="m-2 flex-1 p-2 outline-none"
+								{...register('postImage')}
+								type="text"
+								placeholder="Optional..."
+							/>
+						</div>
+					)}
+
+					{Object.keys(errors).length > 0 && (
+						<div className="space-y-2 p-2 text-red-500">
+							{errors.postTitle?.type === 'required' && (
+								<p>Title is required</p>
+							)}
+							{errors.subreddit?.type === 'required' && (
+								<p>Subreddit is required</p>
+							)}
+						</div>
+					)}
+					{!!watch('postTitle') && (
+						<button
+							type="submit"
+							className="w-full rounded-full bg-blue-400 p-2 text-white "
+						>
+							Create Post
+						</button>
+					)}
+				</div>
+			)}
 		</form>
 	);
 }
